57456d163457da46856a2bfd74bbdac1859dc386
OlegTepesh <olegdevs@gmail.com>
2017-05-26 00:54:46 +0300

    refactor: hunt down excess braces

--- src/Products/Detail/ColorPicker.jsx
+++ src/Products/Detail/ColorPicker.jsx
@@ -4,13 +4,7 @@ import { ColorPicker, Color } from './styled';
 export default props => (
   <ColorPicker>
     {props.colors.map((color, i) => (
-      <Color
-        key={color}
-        color={color}
-        onClick={() => {
-          props.onChange(i);
-        }}
-      />
+      <Color key={color} color={color} onClick={() => props.onChange(i)} />
     ))}
   </ColorPicker>
 );
