250360fc804ef4e7800d6ed14e757c933a97c362
OlegTepesh <olegdevs@gmail.com>
2017-06-12 22:28:40 +0300

    feat: remove filters

--- src/Products/List/Filters/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { StyledButton } from './styled';
-
-const Button = props => (
-  <StyledButton isActive={props.isActive}>{props.children}</StyledButton>
-);
-
-export default Button;
