77bf17e145a284ec803f5dd9113a4eccc23b8956
OlegTepesh <olegdevs@gmail.com>
2017-06-12 22:35:16 +0300

    fix: a few css fixes

--- src/Products/List/styled.js
+++ src/Products/List/styled.js
@@ -1,10 +1,7 @@
 import styled from 'styled-components';
 
 const ProductsGrid = styled.section`
-  padding: 20px 0;
-  @media (max-width: 992px) {
-    padding: 20px 0;
-  }
+  padding: 25px 0;
 `;
 
 export default ProductsGrid;
